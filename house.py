f114944530e520a53cfeb8b16eab6837195d16f6
tevintajeu <tevot191030@gmail.com>
2024-11-15 13:36:31 +0300

    added smaller roof

--- house.py
+++ house.py
@@ -1,3 +1,4 @@
+
 import math
 
 import cairo
@@ -57,7 +58,6 @@ ctx.line_to(595,160)
 ctx.stroke()
 
 #Window in Shade
-ctx.set_line_width(4)
 ctx.translate(-10,0)
 ctx.move_to(120,300)
 ctx.line_to(200,350)
@@ -81,6 +81,62 @@ ctx.line_to(320,265)
 ctx.line_to(320,355)
 ctx.line_to(260,370)
 ctx.close_path()
+ctx.stroke()
+
+
+
+ctx.move_to(165,210)#1
+ctx.line_to(165,155)#4
+ctx.stroke()
+
+# window
+ctx.move_to(173,155)#4
+ctx.line_to(230,143)#6
+ctx.line_to(230,183)#6
+ctx.line_to(173,198)#1
+ctx.line_to(173,155)#4
+
+ctx.move_to(173 +4,155+4)#4
+ctx.line_to(230 - 4,143 + 6)#6
+ctx.line_to(230 -4,183 - 4)#6
+ctx.line_to(173 + 4,198 - 6)#1
+ctx.line_to(173 + 4,155 + 4)#4
+
+ctx.move_to(177,156)
+ctx.line_to(177, 176)
+ctx.stroke()
+
+
+
+# roof
+
+ctx.move_to(165,210)#1
+ctx.line_to(135,160)#2
+ctx.line_to(123,160)#3
+ctx.line_to(160,160)#4
+ctx.line_to(200,120)#5
+ctx.line_to(250,150)#6
+ctx.line_to(254,145)#7
+ctx.line_to(200,113)#8
+ctx.line_to(165,113)#9
+ctx.line_to(123,160)#3
+
+ctx.move_to(165,210)#1
+ctx.line_to(240,187)#6
+ctx.line_to(240,143)#6
+ctx.set_line_width(4)
+
+
+
+
+
+
+
+
+
+
+
+
 ctx.stroke()
 
 surface.write_to_png("house.png")
